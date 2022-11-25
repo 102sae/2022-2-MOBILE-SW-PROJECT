f2cb2ab2a357a0db7517f02410673d22f4f6de9a
lee <doch02@naver.com>
2022-11-25 19:33:38 +0900

    commit

---
+++ app/src/main/java/com/course/mydietapp/AddfoodActivity.java
@@ -0,0 +1,29 @@
+package com.course.mydietapp;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TimePicker;
+
+public class AddfoodActivity {
+    private RatingBar ratingBar;
+    EditText add;
+    EditText count;
+    TimePicker time;
+    EditText place;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addfood);
+
+        ratingBar=findViewById(R.id.ratingBar);
+        add=findViewById(R.id.add);
+        count=findViewById(R.id.Count);
+        time=findViewById(R.id.Time);
+        place=findViewById(R.id.Place);
+
+
+    }
+
+}
