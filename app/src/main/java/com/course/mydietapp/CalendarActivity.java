0f17b6a693e801ad0acdbdc93800bc9d81085996
102sae <95170874+102sae@users.noreply.github.com>
2022-11-25 16:27:35 +0900

    캘린더 날짜 선택

--- app/src/main/java/com/course/mydietapp/CalendarActivity.java
+++ app/src/main/java/com/course/mydietapp/CalendarActivity.java
@@ -1,15 +1,33 @@
 package com.course.mydietapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter;
 import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter;
 
+import org.w3c.dom.Text;
+
+import java.util.Collections;
+
 public class CalendarActivity extends AppCompatActivity {
     private MaterialCalendarView calendarView;
+    public TextView todayTextView;
+    public EditText contextEditText;
+    public String selectDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +35,33 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
 
         calendarView = findViewById(R.id.calendarview);
-
+        // 월 한글로 변경
         calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
-        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
+
+        calendarView.addDecorators(
+                new SaturdayDecorator(),
+                new SundayDecorator()
+        );
+
+        calendarView.addDecorators(new EventDecorator(Color.RED, Collections.singleton(CalendarDay.today())));
+
+
+        todayTextView = findViewById(R.id.todayTextView);
+        contextEditText = findViewById(R.id.contextEditText);
+        calendarView.setOnDateChangedListener((widget, date, selected) -> {
+            int year = date.getYear();
+            int month = date.getMonth()+1;
+            int day = date.getDay();
+
+            String shot_Day = year + "/" + month + "/" + day;
+            Log.i("shot_Day test", shot_Day + "");
+            todayTextView.setText(shot_Day);
+
+        });
     }
+
+
+
+
 
 }
--- app/src/main/java/com/course/mydietapp/MainActivity.java
+++ app/src/main/java/com/course/mydietapp/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(),CalendarActivity.class);
                 startActivity(intent);
             }
+
         });
     }
 }
